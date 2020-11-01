7a408dc71a74dcb039cdda5f8b85061691a02411
rubyfmzk <rubyfmzk@gmail.com>
2020-11-02 00:33:22 +0900

    k-means page

---
+++ src/views/Sabian.vue
@@ -0,0 +1,110 @@
+<template>
+  <router-view :f="f"></router-view>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+
+
+export default {
+  setup() {
+    const f = reactive({
+      getImage: function(image_key){
+        return ';background-image:url(https://opale-sabian.s3-ap-northeast-1.amazonaws.com/base_image/'+(image_key+1).zeroPadding(3)+'.jpg)'
+      },
+      getGrayBackground: function(color){
+        return ';background-color:rgb('+color.int()+','+color.int()+','+color.int()+')'
+      },
+
+      getNearCenters: function(){
+        let centers = this.getLastCenters()
+        let labels = Array(window.data.length).fill(0)
+
+        window.data.forEach((image, image_key)=>{
+
+          let scores = Array(centers.length).fill(0)
+
+          image.forEach((color, row)=>{
+            centers.forEach((center, center_key)=>{
+              scores[center_key] += (color - center[row])**2
+            })
+          })
+
+          labels[image_key] = scores.indexOf(Math.min(...scores))
+        })
+
+        window.current_labels = labels
+      },
+
+      averageCenters: function(){
+        const centers_count = window.default_centers.length
+        const pixel_count = window.default_centers[0].length
+        let divisors = Array(centers_count).fill(0)
+        let centers = [...Array( centers_count )].map(() => Array(pixel_count).fill(0))
+
+        window.data.forEach((image, image_key)=>{
+          const label = window.current_labels[image_key]
+
+          divisors[label] += 1
+          image.forEach((color, row)=>{
+            centers[label][row] += color
+          })
+        })
+
+        centers.forEach((center, center_key)=>{
+          const divisor = divisors[center_key]
+
+          if(divisor === 0) return
+
+          center.forEach((color, row)=>{
+            centers[center_key][row] = centers[center_key][row] / divisor
+          })
+        })
+
+        window.centers_history.push(centers)
+      },
+
+      getLastCenters: function(){
+        return window.centers_history[window.centers_history.length - 1]
+      },
+
+      classifyResults: function(){
+        const centers_count = window.default_centers.length
+        let centers_class = [...Array( centers_count )].map(() => [])
+
+        window.current_labels.forEach((image_key, center_key)=>{
+          centers_class[image_key].push(center_key)
+        })
+
+        window.centers_class = centers_class
+        return centers_class
+      },
+
+    })
+
+    return { f }
+  }
+
+}
+</script>
+
+<style>
+table tr{
+  vertical-align: top;
+  text-align: left;
+}
+.cell{
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  line-height: 0;
+  margin: 0 2px;
+  background-size: contain;
+}
+.cell div{
+  display: inline-block;
+  height: 10px;
+  width: 10px;
+}
+</style>
